86d726f7ab0614bea3cf1919a678b93cd7013967
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2021-11-16 18:12:23 +0800

    避免编译器优化使CPU负载失效

--- user/src/bin/cpu_load.rs
+++ user/src/bin/cpu_load.rs
@@ -15,14 +15,15 @@ static IS_TIMEOUT: AtomicBool = AtomicBool::new(false);
 #[no_mangle]
 pub fn main() -> i32 {
     let mut rng = XorShiftRng::seed_from_u64(0x1020304050607080u64);
+    let mut ret: u64 = 0;
     init_user_trap();
     unsafe {
         uie::set_usoft();
     }
     while !IS_TIMEOUT.load(Relaxed) {
-        let _ = rng.next_u64();
+        ret = rng.next_u64();
     }
-    0
+    ret as i32
 }
 #[no_mangle]
 pub fn soft_intr_handler(_pid: usize, _msg: usize) {
