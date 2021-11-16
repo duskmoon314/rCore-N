142af7d6d1c3e34cb7529532b86301511e672c3b
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2021-11-16 18:09:02 +0800

    用户程序使用release版本

--- os/build.rs
+++ os/build.rs
@@ -7,7 +7,7 @@ fn main() {
     insert_app_data().unwrap();
 }
 
-static TARGET_PATH: &str = "../user/target/riscv64imac-unknown-none-elf/debug/";
+static TARGET_PATH: &str = "../user/target/riscv64imac-unknown-none-elf/release/";
 
 fn insert_app_data() -> Result<()> {
     let mut f = File::create("src/link_app.asm").unwrap();
--- sleep.sh
+++ sleep.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+clear
+tty
 while true; do sleep 1000; done
