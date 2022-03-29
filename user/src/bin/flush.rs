524a9f87473c3ea1376a1e7e2a24508ce4a21089
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2022-03-29 13:47:59 +0800

    let flush wait workers to finish before exit

--- user/src/bin/flush.rs
+++ user/src/bin/flush.rs
@@ -1,10 +1,18 @@
 #![no_std]
 #![no_main]
-use user_lib::spawn;
+use user_lib::{println, spawn, waitpid};
 #[no_mangle]
 pub fn main() -> i32 {
-    for _ in 0..4 {
-        spawn("flush_trace\0");
+    const WORKER_NUM: usize = 4;
+    println!(
+        "Start flushing trace from cache with {} worker processes...",
+        WORKER_NUM
+    );
+    let worker_pid: [usize; WORKER_NUM] =
+        array_init::array_init(|_| spawn("flush_trace\0") as usize);
+    let mut exit_code: i32 = 0;
+    for i in 0..WORKER_NUM {
+        waitpid(worker_pid[i], &mut exit_code);
     }
     0
 }
