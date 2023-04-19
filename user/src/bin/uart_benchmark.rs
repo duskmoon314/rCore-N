6f24ab76f068592c931d626cb451accde63a5865
Gallium70 <52118815+Gallium70@users.noreply.github.com>
2023-04-19 20:15:08 +0800

    clear all traces at the beginning of the benchmark

--- user/src/bin/uart_benchmark.rs
+++ user/src/bin/uart_benchmark.rs
@@ -6,7 +6,7 @@ extern crate user_lib;
 extern crate alloc;
 
 use bitflags::bitflags;
-use user_lib::{send_msg, sleep, spawn, waitpid};
+use user_lib::{send_msg, sleep, spawn, trace::clear_trace, waitpid};
 
 const CPU_LOAD_NUM: usize = 1;
 
@@ -24,6 +24,7 @@ bitflags! {
 
 #[no_mangle]
 pub fn main() -> i32 {
+    clear_trace();
     let cpu_load_pid: [usize; CPU_LOAD_NUM] =
         array_init::array_init(|_| spawn("cpu_load\0") as usize);
     let mut exit_code: i32 = 0;
